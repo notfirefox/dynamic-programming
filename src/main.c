fc33bcf3b3da7bd2a153ceb8ba57cfdbae55c9c2
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-07-06 11:11:47 +0200

    Fix main

--- src/main.c
+++ src/main.c
@@ -4,8 +4,7 @@
 #include "trinomial.h"
 
 int main(int argc, const char *argv[]) {
-  // int result = dynamic_binomial_v2(5, 3);
-  int result = dynamic_binomial_v2(9, 3);
+  int result = dynamic_binomial(5, 3);
   printf("%d\n", result);
 
   return 0;
