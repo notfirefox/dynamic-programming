4e04633b390dfb096e22c9ad0501b141d4809a46
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-07-09 09:16:28 +0200

    Update lcs header

--- src/lcs.h
+++ src/lcs.h
@@ -7,6 +7,3 @@ int dynamic_lcslen(const char[], const char[], const int m, const int n,
 
 void find_lcs(const int m, const int n, const char l, char result[l],
               const char b[m + 1][n + 1], const char x[m]);
-
-void print_lcs(const int m, const int n, const int i, const int j,
-               char b[m + 1][n + 1], const char x[m]);
